03df4f356895b8e3bd6644ddd14dc30ee4642b1f
Vuk Stanojevic <vuki.stanojevic@protonmail.com>
2026-04-01 16:46:56 +1300

    Make it executable

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,17 @@ program
   .name("route-ink")
   .version("0.0.1")
   .description("A CLI tool to generate fully typed API clients from Fastify routes.")
+  .showHelpAfterError()
+  .showSuggestionAfterError();
 
 program
   .command("generate")
-  .description("Generate API client from Fastify routes.")
+  .description("Generate API client from Fastify routes.\n")
   .action(generate)
+
+// Show help if no command provided
+program.action(() => {
+  program.help();
+});
 
 program.parse();
